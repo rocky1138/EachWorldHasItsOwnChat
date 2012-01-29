b1f465a29ac022459c2b3d488d2615f9638988d7
John Rockefeller <jrockefeller1@gmail.com>
2012-01-28 23:35:25 -0500

    Users in one world cannot chat with users in another. Thanks goes to lycano for the help in IRC.

--- src/com/github/rocky1138/ChatListener.java
+++ src/com/github/rocky1138/ChatListener.java
@@ -1,4 +1,8 @@
 package com.github.rocky1138;
+import java.util.LinkedList;
+import java.util.List;
+
+import org.bukkit.entity.Player;
 import org.bukkit.event.Listener;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
@@ -8,6 +12,20 @@ public class ChatListener implements Listener {
 
 	@EventHandler(priority = EventPriority.NORMAL)
 	public void onPlayerChat(final PlayerChatEvent event) {
-		System.out.print("chatted!");
+         
+		if (event.isCancelled())
+            return;
+       
+        Player player = event.getPlayer();
+        String worldName = player.getWorld().getName();
+ 
+        List<Player> recipients = new LinkedList<Player>();
+        for (Player recipient : recipients) {
+            if (recipient.getWorld().getName().equals(worldName))
+                recipients.add(recipient);
+        }
+       
+        event.getRecipients().clear();
+        event.getRecipients().addAll(recipients);
 	}
 }
